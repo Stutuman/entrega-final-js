de501f200c6594cb3a5d31147829e223cd6e80c7
kevin ibanez <la12cabj124@hotmail.com>
2022-12-03 20:56:50 -0300

    agregando objetos

---
+++ objets.js
@@ -0,0 +1,14 @@
+let stockProductos = [
+{id: 1,stock:10 , nombre: "auriculares kotion", precio: 30, img: 'imagesProducts/auricularesKotion.jpg'},
+{id: 2,stock:10 , nombre: "auricularesGreen", precio: 40 , img: 'imagesProducts/auricularGreen.jpg'},
+{id: 3,stock:10 , nombre: "auriculares logitech", precio: 55 , img: 'imagesProducts/auricularLogitech.jpg'},
+{id: 4,stock:10 , nombre: "auricularWhite", precio: 23 , img: 'imagesProducts/auricularWhite.jpg'},
+{id: 5,stock:10 , nombre: "microfono hyperx", precio: 75 , img: 'imagesProducts/microHyperx.jpg'},
+{id: 6,stock:10 , nombre: "microfono mini black", precio: 45 , img: 'imagesProducts/microMiniBlack.jpg'},
+{id: 7,stock:10 , nombre: "microfono mini white", precio: 45 , img: 'imagesProducts/microminiwhite.jpg'},
+{id: 8,stock:10 , nombre: "microfono white", precio: 70  , img: 'imagesProducts/microWhite.jpg'},
+{id: 9,stock:10 ,nombre: "webcam asus", precio: 15 , img: 'imagesProducts/webcamasus.jpg'},
+{id: 10,stock:10 , nombre:  "webcam black", precio: 15 , img: 'imagesProducts/webcamblack.jpg'},
+{id: 11,stock:10 , nombre: "webcam genius", precio: 18 , img: 'imagesProducts/webcamgenius.jpg'},
+{id: 12 ,stock:10 , nombre: "webcam logitech", precio: 28 , img: 'imagesProducts/webcamlogitimagesProducts'},
+]
